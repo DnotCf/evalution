9e870bce00c43985de5e261a70ca9c95b09705af
tang <1398741644@qq.com>
2018-10-11 18:34:04 +0800

    'a'

--- src/main/java/com/myeval/Application.java
+++ src/main/java/com/myeval/Application.java
@@ -3,6 +3,8 @@ package com.myeval;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -17,12 +19,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan(basePackages = "com.myeval")
 @EnableSwagger2
 @Controller
-public class Application {
+public class Application extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
 	}
-	/*@RequestMapping("/hello")
+
+/*@RequestMapping("/hello")
 	@ResponseBody
 	public String hello() {
 		return "hello中文";
